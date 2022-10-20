90e797902d2fb5398d4695a9b5798de676bbff93
solrak <luiscarlosquesada@gmail.com>
2022-10-20 16:34:18 -0700

    Front-Back separation

---
+++ src/api/productos-view/controllers/productos-view.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * productos-view controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::productos-view.productos-view');
---
+++ src/api/productos-view/routes/productos-view.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * productos-view router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::productos-view.productos-view');
